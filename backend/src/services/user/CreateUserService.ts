1ae6311b7f7a256d4d2ea3b00d77b7ec235d508c
Welliton Araujo <welliton.araujo.pb@compasso.com.br>
2022-04-28 15:53:00 -0300

    encrypting password

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,13 +1,50 @@
+import { hash } from 'bcryptjs';
+import prismaClient from '../../prisma'
+
 interface UserRequest{
-    name: String;
-    email: String,
-    password: String;
+  name: string;
+  email: string;
+  password: string;
 }
 
 class CreateUserService{
-    async execute({name, email, password}: UserRequest){
-        return { ok: true }
+  async execute({ name, email, password }: UserRequest){
+
+    // verificar se ele enviou um email
+    if(!email){
+      throw new Error("Email incorrect")
     }
+
+    //Verificar se esse email já está cadastrado na plataforma
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where:{
+        email: email
+      }
+    })
+
+    if(userAlreadyExists){
+      throw new Error("User already exists")
+    }
+
+    const passwordHash = await hash(password, 8)
+
+    // Cadastrar usuario
+    const user = await prismaClient.user.create({
+      data:{
+        name: name,
+        email: email,
+        password: passwordHash,
+      },
+      select: {
+          id: true,
+          name: true,
+          email: true,
+      }
+    })
+
+
+    return user;
+  }
 }
 
 export { CreateUserService }
