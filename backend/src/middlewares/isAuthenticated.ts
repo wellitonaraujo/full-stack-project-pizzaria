f4c915f4624efab943d1c1d8209cf1ba3a98be06
Welliton Araujo <welliton.araujo.pb@compasso.com.br>
2022-04-29 10:53:56 -0300

    validating authentication token

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -1,12 +1,32 @@
 import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
 
-export function isAuthenticated(    
-    req: Request,
-    res: Response,
-    next: NextFunction
-)
-    {
-        console.log('Chamouuu')
+interface PayLoad{
+    sub: string,
+}
 
-        return next()
+// Verifica se esta autenticado
+export function isAuthenticated(req: Request, res: Response, next: NextFunction){
+    const authToken = req.headers.authorization;
+
+    if(!authToken){
+        return res.status(401).end();
     }
+
+    // pegando apenas o token
+    const [, token] = authToken.split(" ");
+
+    try {
+        // validar o token
+        const { sub } = verify(
+            token,
+            process.env.JWT_SECRET
+        ) as PayLoad;
+
+        return next();
+        
+    } catch (error) {
+        return res.status(401).end();
+    }
+
+}
