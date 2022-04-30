f3dc25cd1e9a057dfb86ed8660c64624d7194dee
Welliton Araujo <welliton.araujo.pb@compasso.com.br>
2022-04-30 12:38:13 -0300

    configuring image upload

--- backend/src/controllers/product/CreateProductController.ts
+++ backend/src/controllers/product/CreateProductController.ts
@@ -6,19 +6,25 @@ class CreateProductController{
 
         const {name, price, description, category_id} = req.body;
 
-        const banner = ''
-
         const createProductService = new CreateProductService();
-        
-        const product = await createProductService.execute({
-            name, 
-            price,
-            description,
-            banner, 
-            category_id
-        });
 
-        return res.json(product);
+        if(!req.file) {
+            throw new Error("error upload file");
+        }else{
+
+            const { originalname, filename } = req.file;
+            console.log(originalname)
+        
+            const product = await createProductService.execute({
+                name, 
+                price,
+                description,
+                banner: '', 
+                category_id
+            });
+    
+            return res.json(product);
+        }
     }
 }
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
@@ -10,7 +11,11 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 import { CreateProductController } from './controllers/product/CreateProductController';
 
+import uploadConfig from './config/multer';
+
 const router = Router()
+
+const upload = multer(uploadConfig.upload("./tmp"))
 
 // Rotas USER
 router.post('/users', new CreateUserController().handle)
@@ -22,6 +27,6 @@ router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 // Rotas PRODUCT
-router.post('/product', isAuthenticated, new CreateProductController().handle);
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
 
 export { router };
