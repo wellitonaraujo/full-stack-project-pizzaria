48129c6a0ef2711fc53dd9933018d4fada42ef5a
Welliton Araujo <welliton.araujo.pb@compasso.com.br>
2022-04-30 11:26:57 -0300

    creating product by category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,8 +5,10 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
+
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/product/CreateProductController';
 
 const router = Router()
 
@@ -18,5 +20,8 @@ router.get('/detail', isAuthenticated, new DetailUserController().handle);
 // Rotas CATEGORY
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+// Rotas PRODUCT
+router.post('/product', isAuthenticated, new CreateProductController().handle);
 
 export { router };
