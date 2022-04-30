d08b3a0a24c9a8a92e8bc1217785187328d11e99
Welliton Araujo <welliton.araujo.pb@compasso.com.br>
2022-04-30 15:50:42 -0300

    adding items to the table

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -15,6 +15,7 @@ import uploadConfig from './config/multer';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
 
 const router = Router()
 
@@ -36,5 +37,6 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // Rotas ORDER
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+router.post('/order/add', isAuthenticated, new AddItemController().handle);
 
 export { router };
