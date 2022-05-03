5b49dc780a6ede90d59a4accbbfb8fa730b6a8e9
Welliton Araujo <welliton.araujo.pb@compasso.com.br>
2022-05-03 11:44:19 -0300

    frontend init

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -22,6 +22,7 @@ import { SendOrderController } from './controllers/order/SendOrderController';
 import { ListOrderController } from './controllers/order/ListOrderController';
 import { DetailOrderController } from './controllers/order/DetailOrderController';
 import { FinishOrderController } from './controllers/order/FinishOrderController';
+import { RemoveCategoryController } from './controllers/category/RemoveCategoryController';
 
 const router = Router()
 
@@ -49,5 +50,6 @@ router.put('/order/send', isAuthenticated, new SendOrderController().handle);
 router.get('/orders', isAuthenticated, new ListOrderController().handle);
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle);
 router.put('/order/finish', isAuthenticated, new FinishOrderController().handle);
+router.delete('/category/remove', isAuthenticated, new RemoveCategoryController().handle);
 
 export { router };
