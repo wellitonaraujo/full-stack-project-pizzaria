e5d21b2b4e99eb6e785f0136643aa1c52287bd1c
Welliton Araujo <welliton.araujo.pb@compasso.com.br>
2022-05-03 14:05:53 -0300

    create input component

---
+++ frontend/src/components/ui/Input/index.tsx
@@ -0,0 +1,18 @@
+import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react';
+
+import styles from './styles.module.scss';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{}
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement>{}
+
+export function Input({...rest}: InputProps){
+    return(
+        <input type="text" className={styles.input} {...rest} />
+    )
+}
+
+export function TextArea({...rest}: TextAreaProps){
+    return(
+        <textarea className={styles.input} {...rest} />
+    )
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,34 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import logoImg from '../../public/logo.svg';
+import styles from '../../styles/home.module.scss';
+import { Input } from "../components/ui/Input";
+
 export default function Home() {
   return (
-    <div>
-        <h1>Ola</h1>
-    </div>
+    <>
+    <Head>
+      <title>Pizzaria - Login</title>
+    </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Pizzaria"/>
+
+        <div className={styles.login}>
+          <form>
+            <Input 
+              placeholder="Digite seu email"
+              type="email"
+            />
+
+            <Input 
+              placeholder="Sua senha"
+              type="password"
+            />
+          </form>
+        </div>
+
+      </div>
+    </>
   )
 }
