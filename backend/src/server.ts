88dd815fd039f323946e0eb609f4ffeb5d79dda0
Welliton Araujo <welliton.araujo.pb@compasso.com.br>
2022-05-11 13:46:25 -0300

    ...

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,17 +6,13 @@ import path from 'path';
 import { router } from "./routes";
 
 const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use(router);
-
-app.use(
-    '/files',
-    express.static(path.resolve(__dirname, '..', 'tmp'))
-)
 
 const port = 33333;
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if(err instanceof Error) {
@@ -32,4 +28,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     })
 })
 
-app.listen(port, () => console.log('Servidor rodando....'))
+app.listen(port, () => console.log('Servidor rodando...'))
